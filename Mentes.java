feba2f1a668d4e9e31056bb0869b4090c08b17aa
czabalab <113118849+czabalab@users.noreply.github.com>
2023-05-28 14:09:20 +0200

    Mentes.java kiegészítve játék végi opcióval

--- Mentes.java
+++ Mentes.java
@@ -1,20 +1,24 @@
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
-public class Mentes
-{
-    public static void mentesEredmenyek(String eredmenyek)
-    {
-        try
-        {
-            FileWriter writer = new FileWriter("eredmenyek.txt");
-            writer.write(eredmenyek);
-            writer.close();
-            System.out.println("Az eredmények sikeresen mentve lettek az eredmenyek.txt fájlba.");
-        } 
-        catch (IOException e)
-        {
-            System.out.println("Hiba történt a fájl mentése során: " + e.getMessage());
+public class Mentes {
+    public static void mentesEredmenyek(String eredmenyek) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Szeretnéd menteni az eredményeket? (igen/nem)");
+        String valasz = scanner.nextLine();
+
+        if (valasz.equalsIgnoreCase("igen")) {
+            try {
+                FileWriter writer = new FileWriter("eredmenyek.txt");
+                writer.write(eredmenyek);
+                writer.close();
+                System.out.println("Az eredmények sikeresen mentve lettek az eredmenyek.txt fájlba.");
+            } catch (IOException e) {
+                System.out.println("Hiba történt a fájl mentése során: " + e.getMessage());
+            }
+        } else {
+            System.out.println("Az eredmények nem lettek mentve.");
         }
     }
 }
